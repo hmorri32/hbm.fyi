d896b4e1fa62381c05c8b4bbb2923b777bf86235
hmorri32 <hmorrison@fastly.com>
2022-01-25 16:37:58 -0500

    pulls in some css for hover state purposes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,28 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Footer from "../components/footer"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import Footer from "../components/footer";
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
+import "./index.css";
 
 const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const posts = data.allMarkdownRemark.edges
+  const siteTitle = data.site.siteMetadata.title;
+  const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="hbm.fyi" />
       <Bio />
       {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
+        const title = node.frontmatter.title || node.fields.slug;
         return (
           <article key={node.fields.slug}>
             <header>
               <h3
+                className="blog-title"
                 style={{
                   marginBottom: rhythm(1 / 4),
                 }}
@@ -29,24 +31,24 @@ const BlogIndex = ({ data, location }) => {
                   {title}
                 </Link>
               </h3>
-              <small>{node.frontmatter.date}</small>
+              <small>Updated: {node.frontmatter.date}</small>
             </header>
             <section>
               <p
                 dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
+                  __html: node.frontmatter.description || "",
                 }}
               />
             </section>
           </article>
-        )
+        );
       })}
       <Footer />
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogIndex
+export default BlogIndex;
 
 export const pageQuery = graphql`
   query {
@@ -71,4 +73,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
