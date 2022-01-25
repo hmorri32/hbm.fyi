ce392dea46b914ae1db411cfada85666afd70df8
hmorri32 <hmorrison@fastly.com>
2022-01-25 16:35:04 -0500

    rips GH component from bio, adds location data

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,6 +3,16 @@ import profile from "../assets/profile.jpeg";
 import { rhythm } from "../utils/typography";
 
 class Bio extends Component {
+  state = { hover: false };
+
+  handleMouseEnter() {
+    this.setState({ hover: true });
+  }
+
+  handleMouseLeave() {
+    this.setState({ hover: false });
+  }
+
   render() {
     return (
       <div
@@ -24,7 +34,18 @@ class Bio extends Component {
           }}
         />
         <p style={{ maxWidth: 310, margin: 0, padding: 0 }}>
-          <a href="https://github.com/hmorri32">Hugh Morrison</a>
+          Hugh Morrison
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
+            </svg>
+            Asheville, NC
+          </p>
         </p>
       </div>
     );
